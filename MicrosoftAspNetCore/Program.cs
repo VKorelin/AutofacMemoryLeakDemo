89ae1e405f387c07b660597bd539555413f338fb
v.korelin <v.korelin@fxpro.com>
2023-12-02 16:08:11 +0200

    Change Program.cs

--- MicrosoftAspNetCore/Program.cs
+++ MicrosoftAspNetCore/Program.cs
@@ -4,10 +4,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Host
-            .CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+        CreateHostBuilder(args)
             .Build()
             .Run();
     }
+    
+    private static IHostBuilder CreateHostBuilder(string[] args)
+        => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
 }
